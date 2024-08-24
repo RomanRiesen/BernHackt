d3783eb01cdd292de6418f47847e19697b930b58
RomanRiesen <14959627+RomanRiesen@users.noreply.github.com>
2024-08-24 03:17:14 +0200

    added RiskGameEvent.vue though that is really stupid because nothing works

---
+++ risk-rally/src/components/RiskGameEvent.vue
@@ -0,0 +1,11 @@
+<script setup>
+const props = defineProps(['fired'])
+</script>
+<template>
+    <div>
+        {{ fired.event }}
+    </div>
+    <div>
+        was in CHF {{ fired.actual_damages }} Kosten resultiert.
+    </div>
+</template>
