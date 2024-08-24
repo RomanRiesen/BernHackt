e4fbcd89ce77b789bc2c357b01a225d192a98df2
Leon S <27723630+leonschmidlin@users.noreply.github.com>
2024-08-24 22:36:07 +0200

    Finally fixed the clogged up CSS. Mismatched merge action realigned.

--- risk-rally/src/views/MiniGame.vue
+++ risk-rally/src/views/MiniGame.vue
@@ -4,7 +4,12 @@
     <h1>Mini Game: Save the House!</h1>
     <div class="game-container">
       <div ref="house" class="house" :style="houseStyle"></div>
-      <div v-for="(storm, index) in storms" :key="index" class="storm" :style="storm.style"></div>
+      <div
+        v-for="(storm, index) in storms"
+        :key="index"
+        class="storm"
+        :style="storm.style"
+      ></div>
     </div>
     <p v-if="gameOver">Game Over! You survived for {{ score }} seconds.</p>
     <button @click="startGame" v-if="!isPlaying">Start Game</button>
@@ -20,79 +25,78 @@ export default {
       isPlaying: false,
       gameOver: false,
       score: 0,
-      intervalId: null
-    }
+      intervalId: null,
+    };
   },
   computed: {
     houseStyle() {
       return {
-        left: this.housePosition + '%'
-      }
-    }
+        left: this.housePosition + "%",
+      };
+    },
   },
   methods: {
     startGame() {
-      this.isPlaying = true
-      this.gameOver = false
-      this.score = 0
-      this.storms = []
-      this.intervalId = setInterval(this.spawnStorm, 1000)
-      this.moveStorms()
-      this.increaseScore()
-      document.addEventListener('keydown', this.moveHouse)
+      this.isPlaying = true;
+      this.gameOver = false;
+      this.score = 0;
+      this.storms = [];
+      this.intervalId = setInterval(this.spawnStorm, 1000);
+      this.moveStorms();
+      this.increaseScore();
+      document.addEventListener("keydown", this.moveHouse);
     },
     moveHouse(event) {
-      if (event.key === 'ArrowLeft' && this.housePosition > 0) {
-        this.housePosition -= 5
-      } else if (event.key === 'ArrowRight' && this.housePosition < 90) {
-        this.housePosition += 5
+      if (event.key === "ArrowLeft" && this.housePosition > 0) {
+        this.housePosition -= 5;
+      } else if (event.key === "ArrowRight" && this.housePosition < 90) {
+        this.housePosition += 5;
       }
     },
     spawnStorm() {
-      const stormPosition = Math.floor(Math.random() * 90) // Random horizontal position
-      this.storms.push({ style: { left: stormPosition + '%', top: '0%' } })
+      const stormPosition = Math.floor(Math.random() * 90); // Random horizontal position
+      this.storms.push({ style: { left: stormPosition + "%", top: "0%" } });
     },
     moveStorms() {
       setInterval(() => {
         this.storms = this.storms.map((storm) => {
-          const newTop = parseInt(storm.style.top) + 5
+          const newTop = parseInt(storm.style.top) + 5;
           if (newTop > 90 && this.checkCollision(storm.style.left, newTop)) {
-            this.endGame()
+            this.endGame();
           }
-          return { style: { ...storm.style, top: newTop + '%' } }
-        })
-      }, 100)
+          return { style: { ...storm.style, top: newTop + "%" } };
+        });
+      }, 100);
     },
     checkCollision(stormLeft, stormTop) {
-      const stormLeftNumber = parseInt(stormLeft)
-      return stormTop > 80 && Math.abs(stormLeftNumber - this.housePosition) < 10
+      const stormLeftNumber = parseInt(stormLeft);
+      return stormTop > 80 && Math.abs(stormLeftNumber - this.housePosition) < 10;
     },
     increaseScore() {
       setInterval(() => {
         if (this.isPlaying) {
-          this.score += 1
+          this.score += 1;
         }
-      }, 1000)
+      }, 1000);
     },
     endGame() {
-      this.isPlaying = false
-      this.gameOver = true
-      clearInterval(this.intervalId)
-      document.removeEventListener('keydown', this.moveHouse)
-    }
+      this.isPlaying = false;
+      this.gameOver = true;
+      clearInterval(this.intervalId);
+      document.removeEventListener("keydown", this.moveHouse);
+    },
   },
   beforeUnmount() {
-    clearInterval(this.intervalId)
-    document.removeEventListener('keydown', this.moveHouse)
-  }
-}
+    clearInterval(this.intervalId);
+    document.removeEventListener("keydown", this.moveHouse);
+  },
+};
 </script>
 
 <style scoped>
 .game-container {
-  position: absolute;
-  width: 23%;
-  left: 740px;
+  position: relative;
+  width: 100%;
   height: 400px;
   background-color: #87ceeb;
   overflow: hidden;
@@ -116,6 +120,6 @@ export default {
 }
 
 button:hover {
-  background-color: #FE8E18;
+  background-color: #fe8e18;
 }
 </style>
